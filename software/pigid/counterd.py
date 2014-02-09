d60765f215068acc43348f4f08b98ab7b8748170
Sebastian Steuer <iggy@yggi.de>
2014-02-09 21:47:16 +0100

    webserver working again on non-pi devices

--- software/pigid/counterd.py
+++ software/pigid/counterd.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+    gpio_available = True
+except ImportError:
+    gpio_available = False
+    print "no GPIO available, simulating"
+    
 import time
 import json
 GPIO_PIGI = 4
@@ -10,10 +16,11 @@ ws = None
 
 def counter_start(socket=None):
     #GPIO setup
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIO_PIGI,GPIO.IN)
-    GPIO.add_event_detect(GPIO_PIGI,GPIO.FALLING)
-    GPIO.add_event_callback(GPIO_PIGI,countEventHandler)
+    if gpio_available:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(GPIO_PIGI,GPIO.IN)
+        GPIO.add_event_detect(GPIO_PIGI,GPIO.FALLING)
+        GPIO.add_event_callback(GPIO_PIGI,countEventHandler)
     global ws
     ws = socket
     

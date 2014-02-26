8da250fa0beeda39f6a697580670168a57cd224a
Sebastian Steuer <iggy@yggi.de>
2014-02-26 23:14:31 +0100

    nicer format string for history

--- software/pigid/public/assets/js/webGI.js
+++ software/pigid/public/assets/js/webGI.js
@@ -304,7 +304,7 @@ function updateHistory(data) {
         backgroundColor: "rgba(13,12,8,0.25)",
         title:{ text: "All time uSv/h", fontSize: 14, horizontalAlign: "right", fontColor: "rgba(117,137,12,0.8)", margin: 8 },
         axisY:{ minimum: 0, labelFontFamily: "Digi", gridThickness: 1, gridColor: "rgba(216,211,197,0.1)", lineThickness: 0, tickThickness: 0, interlacedColor: "rgba(216,211,197,0.05)"  },
-        axisX:{ valueFormatString: "HH:mm", labelAngle: 0, labelFontFamily: "Digi", gridThickness: 1, gridColor: "rgba(216,211,197,0.1)", lineThickness: 1, tickThickness: 1 },
+        axisX:{ valueFormatString: "MM-DD", labelAngle: 0, labelFontFamily: "Digi", gridThickness: 1, gridColor: "rgba(216,211,197,0.1)", lineThickness: 1, tickThickness: 1 },
         data: [{ type: "area", color: "rgba(117,137,12,0.8)", dataPoints: webGI.history.data.doserate },
                { type: "line", color: "rgba(210,242,30,0.6)", dataPoints: webGI.history.data.doserate_avg }]
     });

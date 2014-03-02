f6d5ed9904a99aa43974f616f2196cfa156515c6
Sebastian Steuer <iggy@yggi.de>
2014-03-02 21:25:52 +0100

    logging to file

--- software/pyGIserver.py
+++ software/pyGIserver.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
 import logging
-
 from pyGI.configurator import cfg
 
-logging.basicConfig(
-    level=cfg.get('logging','level'),
-    format='%(asctime)s %(levelname)s %(name)s: %(message)s'
-)
-log = logging.getLogger("pyGIserver")
-#log.addHandler(logging.StreamHandler())
-#if cfg.getboolean('logging','write_file'):
-#    log.addHandler(logging.FileHandler(cfg.get('logging','filename')))
-
-log.info("Starting pyGIserver")
-
-from pyGI import geigercounter,geigerserver,geigerlog
+#setup logging
+log = logging.getLogger()
+log.setLevel(cfg.get('logging','level'))
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+if cfg.getboolean('logging','write_file'):
+    filehandler = logging.FileHandler(cfg.get('logging','filename'))
+    filehandler.setFormatter(formatter)
+    log.addHandler(filehandler)
+streamhandler = logging.StreamHandler()
+streamhandler.setFormatter(formatter)
+log.addHandler(streamhandler)
 
 if __name__ == "__main__":
-    last_total = geigerlog.get_last_totalcount()
-    geiger = geigercounter.Geigercounter(total=last_total)
-    geigerlog = geigerlog.GeigerLog(geiger)
+    log.info("Starting pyGIserver")
+    from pyGI import geigercounter,geigerserver,geigerlog
     try:
+        #get last totalcount from db
+        last_total = geigerlog.get_last_totalcount()
+        
+        #start geigercounter
+        geiger = geigercounter.Geigercounter(total=last_total)
+        
+        #start geigercounter logging
+        geigerlog = geigerlog.GeigerLog(geiger)
+    
+        #start the bottle server stuff
         geigerserver.start(geiger,geigerlog)
+        
     except KeyboardInterrupt:
         log.info("Stopping pyGIserver")

f809bd88cc8148aef44bd18bb48650edb1d6c939
chrono <chrono@open-resource.org>
2014-05-08 13:51:16 +0000

    Chart fixes

--- software/public/assets/js/webGI_history.js
+++ software/public/assets/js/webGI_history.js
@@ -21,46 +21,42 @@ webGI.history = (function($) {
 
     //Public Function
     my.init = function() {
-        if (data.length === 0)
-        {
+        if (data.length === 0) {
             return;
         }
 
-        var clickCallback = function(e)
-        {
+        var clickCallback = function(e) {
             var x = e.offsetX;
             var y = e.offsetY;
             var dataXY = chart.toDataCoords(x, y);
             $('#eventTS').html(new Date(dataXY[0]));
             $('#eventText').val("Enter your annotation text here...");
-            webGI.livechart.annotation_ts = dataXY[0]/1000;
-            console.log("HA:",webGI.livechart.annotation_ts);
+            webGI.livechart.annotation_ts = dataXY[0] / 1000;
+            console.log("HA:", webGI.livechart.annotation_ts);
             $('#eventEDR').html(dataXY[1].toFixed(2));
             $('#modalAnnotation').addClass('md-show');
         };
 
-        var annotationClickCallback = function(annotation, point)
-        {
+        var annotationClickCallback = function(annotation, point) {
             console.log(annotation.xval);
             console.log(annotation.text);
             console.log(point.yval);
             console.log(point.yval);
-            webGI.livechart.annotation_ts = annotation.xval/1000;
-            console.log("HAA:",webGI.livechart.annotation_ts);
+            webGI.livechart.annotation_ts = annotation.xval / 1000;
+            console.log("HAA:", webGI.livechart.annotation_ts);
             $('#eventTS').html(new Date(annotation.xval));
             $('#eventEDR').html(point.yval.toFixed(2));
             $('#eventText').val(annotation.text);
             $('#modalAnnotation').addClass('md-show');
         };
 
-        chart = new Dygraph(my.container_id, data,
-        {
+        chart = new Dygraph(my.container_id, data, {
             //interactionModel : {
             //    'mousedown' : Dygraph.Interaction.startPan,
             //    'mousemove' : Dygraph.Interaction.movePan,
             //    'mouseup' : Dygraph.Interaction.endPan,
-                //'click' : clickV3,
-                //'dblclick' : dblClickV3,
+            //'click' : clickV3,
+            //'dblclick' : dblClickV3,
             //    'mousewheel' : Dygraph.Interaction.moveZoom
             //},
             showRangeSelector: true,
@@ -71,25 +67,25 @@ webGI.history = (function($) {
             rightGap: 10,
             fillAlpha: 0.7,
             fillGraph: true,
-            showRoller: true,
-            valueRange: [0.01,null],
+            showRoller: false,
+            valueRange: [0.01, null],
             //yRangePad: 10,
-            drawCallback: function(dygraph, initial)
-            {
+            drawCallback: function(dygraph, initial) {
                 var range = dygraph.yAxisRange();
-                if (range[0] != 0.01)
-                {
-                    console.log("Fixing range",range);
+                if (range[0] != 0.01) {
+                    console.log("Fixing range", range);
                     range[0] = 0.01;
                     range[1] = null; //range[1]*2;
-                    dygraph.updateOptions({valueRange: range});
+                    dygraph.updateOptions({
+                        valueRange: range
+                    });
                 }
             },
             //zoomCallback: function(min,max,y) {
             //    webGI.history.chart.updateOptions({valueRange: [0.01, null]});
             //},
             interactionModel: {
-                'click' : clickCallback
+                'click': clickCallback
             },
             annotationClickHandler: annotationClickCallback,
             /*
@@ -101,70 +97,62 @@ webGI.history = (function($) {
             },*/
             //includeZero: true,
             //connectSeparatedPoints: true,
-            labels: ['time','µSv/h','µSv/h (15m avg)'],
+            labels: ['time', 'µSv/h', 'µSv/h (15m avg)'],
             xlabel: 'time',
-            xLabelHeight : 25,
-            colors: ['#677712','yellow'],
-            'µSv/h':
-            {
+            xLabelHeight: 25,
+            colors: ['#677712', 'yellow'],
+            'µSv/h': {
                 fillGraph: true,
                 stepPlot: true,
             },
-            'µSv/h (15m avg)':
-            {
+            'µSv/h (15m avg)': {
                 fillGraph: false,
             },
         });
         chart.setAnnotations(annotations);
     };
 
-
-
-
     my.update = function(msg) {
         //console.log("HISTORY");
         data = [];
         annotations = [];
-        $.each(msg.log, function(i,v)
-        {
+        $.each(msg.log, function(i, v) {
             //var v = JSON.parse(v_json);
-            var ts = new Date(v.timestamp*1000);
-            if (isNaN(ts.getTime()))
-            {
+            var ts = new Date(v.timestamp * 1000);
+            if (isNaN(ts.getTime())) {
                 return;
             }
-            if(v.annotation !== "") {
-                annotations.push( {
-                  series: 'µSv/h',
-                  x: v.timestamp*1000,
-                  shortText: v.annotation[0],
-                  text: v.annotation
-                } );
+            if (v.annotation !== "") {
+                annotations.push({
+                    series: 'µSv/h',
+                    x: v.timestamp * 1000,
+                    shortText: v.annotation[0],
+                    text: v.annotation
+                });
             }
-            data.push([ts,v.data.edr,v.data.edr_avg]);
+            data.push([ts, v.data.edr, v.data.edr_avg]);
         });
 
-        if (webGI.history.chart === null)
-        {
+        if (!webGI.history.chart) {
             my.init();
-        }
-        else
-        {
-            chart.updateOptions({ file: webGI.history.data });
+        } else {
+            chart.updateOptions({
+                file: webGI.history.data
+            });
             chart.setAnnotations(annotations);
         }
 
     };
 
     my.set_log_scale = function(enabled) {
-        chart.updateOptions({ logscale: enabled });
+        chart.updateOptions({
+            logscale: enabled
+        });
         my.log_scale = enabled;
     };
 
-
     //Private Function
 
-
-    //Do not forget to return my, otherwise nothing will work.
     return my;
-}($));  //Pass jq/zepto to the module construction function call
+
+}($));
--- software/public/assets/js/webGI_livechart.js
+++ software/public/assets/js/webGI_livechart.js
@@ -1,20 +1,20 @@
-//Create webGI object if neccessary
+// Create webGI object if neccessary
 if (typeof webGI === 'undefined') {
     webGI = {};
 }
 
-//Add module to webGI namespace
+// Add livechart module to webGI namespace
 webGI.livechart = (function($) {
-    //We have jquery/zepto available ($)
 
-    //Public attributes
-    var my = {};
-    my.container_id = "chartContainer";
-    my.chart_age = 60*15;
-    my.now = Date.now();
-    my.annotation_ts = null;
+    // Public attributes
+    var my = {
+        container_id : 'chartContainer',
+        chart_age : 60 * 15,
+        now : Date.now(),
+        annotation_ts : null
+    };
 
-    //Private attributes
+    // Private attributes
     var container = null;
     var chart = null;
     var data = [];
@@ -24,24 +24,106 @@ webGI.livechart = (function($) {
     var desired_range = null;
     var animtimer = null;
     var annotations = [];
-    var edr_avg_24h = 0.1; //FIXME this does not belong here
-    var chart_colors = ['#677712','yellow'];
+    var edr_avg_24h = 0.1; // FIXME this does not belong here
+    var chart_colors = ['#677712', 'yellow'];
     var ws_log = null;
 
+    // Public functions
+
+    my.init = function() {
+
+        container = $("#" + my.container_id);
+
+        function objToString(obj) {
+            var tabjson = [];
+            for (var p in obj) {
+                if (obj.hasOwnProperty(p)) {
+                    tabjson.push('"' + p + '"' + ':' + obj[p]);
+                }
+            }
+            tabjson.push();
+            return '{' + tabjson.join(',') + '}';
+        }
+
+        //console.log("Init log");
+        if (data.length === 0) {
+            return;
+        }
+
+        var clickCallback = function(e) {
+            var x = e.offsetX;
+            var y = e.offsetY;
+            var dataXY = chart.toDataCoords(x, y);
+            alert(objToString(e));
+            $('#eventTS').html(new Date(parseInt(dataXY[0])));
+            $('#eventText').val("Enter your annotation text here...");
+            my.annotation_ts = dataXY[0] / 1000;
+            $('#eventEDR').html(dataXY[1].toFixed(2));
+            $('#modalAnnotation').addClass('md-show');
+        };
+
+        var annotationClickCallback = function(annotation, point) {
+            console.log(annotation.xval);
+            console.log(annotation.text);
+            console.log(point.yval);
+            console.log(point.yval);
+            my.annotation_ts = annotation.xval / 1000;
+            $('#eventTS').html(new Date(annotation.xval));
+            $('#eventEDR').html(point.yval.toFixed(2));
+            $('#eventText').val(annotation.text);
+            $('#modalAnnotation').addClass('md-show');
+        };
+
+        my.save_annotation = function() {
+            var annotation_text = $('#eventText').val();
+            console.log(my.annotation_ts, annotation_text);
+            my.pushAnnotation(my.annotation_ts, annotation_text);
+            my.requestLog(60 * 60 * 24, false);
+        };
+
+        chart = new Dygraph(my.container_id, data, {
+            dateWindow: [my.now - my.chart_age * 1000, my.now],
+            title: 'EAR: $$ uSv/h (AVG) - EAD: $$ uSv (Total)',
+            titleHeight: 35,
+            fillGraph: true,
+            rightGap: 20,
+            fillAlpha: 0.7,
+            showRoller: false,
+            rollPeriod: 1,
+            interactionModel: {
+                'click': clickCallback
+            },
+            annotationClickHandler: annotationClickCallback,
+            //valueRange: [0,null],
+            includeZero: true,
+            animatedZooms: true,
+            labels: ['time', 'µSv/h', 'µSv/h (15m avg)'],
+            xlabel: 'time',
+            colors: chart_colors,
+            'µSv/h': {
+                fillGraph: true,
+                stepPlot: true,
+            },
+            'µSv/h (15m avg)': {
+                fillGraph: false,
+            }
+        });
+    };
+
     my.init_socket = function() {
-        ws_log = new WebSocket(webGI.conf.websocket_host+"/ws_log");
+        ws_log = new WebSocket(webGI.conf.websocket_host + "/ws_log");
 
         ws_log.onopen = function() {
             $('#modalError').removeClass('md-show');
-            setTimeout(function(){
-                my.requestLog(60*60*1,true);
-                my.requestLog(60*60*24,false);
-                my.requestHistory(null,null);
+            setTimeout(function() {
+                my.requestLog(60 * 60 * 1, true);
+                my.requestLog(60 * 60 * 24, false);
+                my.requestHistory(null, null);
             }, 100);
         };
 
         ws_log.onclose = function() {
-            ws_log = new WebSocket(webGI.conf.websocket_host+"/ws_log");
+            ws_log = new WebSocket(webGI.conf.websocket_host + "/ws_log");
             showErrorModal(
                 'Websocket Error',
                 '<p>Wheeeeh, I lost my sockets. Either the server has gone down or the network connection is unreliable or stalled.</p><b>Possible solutions:</b></p><ul><li>Is the pyGI daemon running on the Pi?</li><li>Enable/toggle your WIFI connection</li></ul>'
@@ -52,190 +134,103 @@ webGI.livechart = (function($) {
         ws_log.onmessage = function(e) {
             var msg = JSON.parse(e.data);
             //console.log(msg);
-            switch(msg.type) {
+            switch (msg.type) {
                 case "geigerjson":
                     my.update(msg);
-                break;
+                    break;
                 case "history":
                     my.updateBacklog(msg);
-                break;
+                    break;
                 case "static_history":
                     webGI.history.update(msg);
-                break;
+                    break;
                 default:
-                    console.log("INVALID MESSAGE",msg);
+                    console.log("INVALID MESSAGE", msg);
             }
         };
-    };
-
-    //Public Function
-    my.init = function() {
-        container = $("#"+my.container_id);
-
-function objToString (obj) {
-    var tabjson=[];
-    for (var p in obj) {
-        if (obj.hasOwnProperty(p)) {
-            tabjson.push('"'+p +'"'+ ':' + obj[p]);
-        }
-    }  tabjson.push();
-    return '{'+tabjson.join(',')+'}';
-}
-
-        //console.log("Init log");
-        if (data.length===0)
-        {
-            return;
-        }
-
-        var clickCallback = function(e)
-        {
-            var x = e.offsetX;
-            var y = e.offsetY;
-            var dataXY = chart.toDataCoords(x, y);
-            alert(objToString(e));
-            $('#eventTS').html(new Date(parseInt(dataXY[0])));
-            $('#eventText').val("Enter your annotation text here...");
-            my.annotation_ts = dataXY[0]/1000;
-            $('#eventEDR').html(dataXY[1].toFixed(2));
-            $('#modalAnnotation').addClass('md-show');
-        };
-
-        var annotationClickCallback = function(annotation, point)
-        {
-            console.log(annotation.xval);
-            console.log(annotation.text);
-            console.log(point.yval);
-            console.log(point.yval);
-            my.annotation_ts = annotation.xval/1000;
-            $('#eventTS').html(new Date(annotation.xval));
-            $('#eventEDR').html(point.yval.toFixed(2));
-            $('#eventText').val(annotation.text);
-            $('#modalAnnotation').addClass('md-show');
-        };
-
-        my.save_annotation = function() {
-            var annotation_text = $('#eventText').val();
-            console.log(my.annotation_ts,annotation_text);
-            my.pushAnnotation(my.annotation_ts,annotation_text);
-            my.requestLog(60*60*24,false);
-        };
-
-        chart = new Dygraph(my.container_id, data,
-        {
-            dateWindow: [ my.now - my.chart_age*1000, my.now],
-            title: 'EAR: $$ uSv/h (AVG) - EAD: $$ uSv (Total)',
-            titleHeight: 35,
-            fillGraph: true,
-            rightGap: 20,
-            fillAlpha: 0.7,
-            showRoller: false,
-            rollPeriod: 1,
-            interactionModel: {
-                'click' : clickCallback
-            },
-            annotationClickHandler: annotationClickCallback,
-            //valueRange: [0,null],
-            includeZero: true,
-            animatedZooms: true,
-            labels: ['time','µSv/h','µSv/h (15m avg)'],
-            xlabel: 'time',
-            colors: chart_colors,
-            'µSv/h':
-            {
-                fillGraph: true,
-                stepPlot: true,
-            },
-            'µSv/h (15m avg)':
-            {
-                fillGraph: false,
-            }
-        });
     };
 
     my.update = function(msg) {
         //console.log("UPDATE")
 
-        var ts = new Date(msg.timestamp*1000);
-        data_hd.push([ts,msg.data.edr,msg.data.edr_avg]);
+        var ts = new Date(msg.timestamp * 1000);
+        data_hd.push([ts, msg.data.edr, msg.data.edr_avg]);
 
         //FIXME: push ld data less often
-        data_ld.push([ts,msg.data.edr,msg.data.edr_avg]);
+        data_ld.push([ts, msg.data.edr, msg.data.edr_avg]);
         data_ld_all.push(msg);
 
-        var left_end_ld = new Date((msg.timestamp-60*60*24)*1000);
-        while(data_ld[0][0] < left_end_ld) data_ld.shift();
-        while(data_ld_all[0].timestamp < msg.timestamp-60*60*24) data_ld_all.shift();
+        var left_end_ld = new Date((msg.timestamp - 60 * 60 * 24) * 1000);
+        while (data_ld[0][0] < left_end_ld) data_ld.shift();
+        while (data_ld_all[0].timestamp < msg.timestamp - 60 * 60 * 24) data_ld_all.shift();
 
-        var left_end_hd = new Date((msg.timestamp-60*60*1)*1000);
-        while(data_hd[0][0] < left_end_hd) data_hd.shift();
+        var left_end_hd = new Date((msg.timestamp - 60 * 60 * 1) * 1000);
+        while (data_hd[0][0] < left_end_hd) data_hd.shift();
 
 
         chart.updateOptions({
             file: data,
-            dateWindow: [ my.now - my.chart_age*1000, my.now]
+            dateWindow: [my.now - my.chart_age * 1000, my.now]
         });
     };
 
-    my.updateBacklog = function(msg){
+    my.updateBacklog = function(msg) {
         //console.log("LOGHISTORY");
         if (msg.hd) {
             data_hd = [];
-            $.each(msg.log, function(i,v)
-            {
-                var ts = new Date(v.timestamp*1000);
+            $.each(msg.log, function(i, v) {
+                var ts = new Date(v.timestamp * 1000);
                 //var ts = v.timestamp*1000
                 //if (isNaN(ts.getTime()))
                 //{
                 //    return;
                 //}
-                data_hd.push([ts,v.data.edr,v.data.edr_avg]);
+                data_hd.push([ts, v.data.edr, v.data.edr_avg]);
             });
         } else {
             data_ld_all = msg.log;
             data_ld = [];
-            var edr_avg=0;
+            var edr_avg = 0;
             annotations = [];
-            $.each(msg.log, function(i,v)
-            {
-                var ts = new Date(v.timestamp*1000);
+            $.each(msg.log, function(i, v) {
+                var ts = new Date(v.timestamp * 1000);
                 //var ts = v.timestamp*1000
                 //if (isNaN(ts.getTime()))
                 //{
                 //    return;
                 //}
-                if(v.annotation !== "") {
-                    annotations.push( {
-                      series: 'µSv/h',
-                      x: v.timestamp*1000,
-                      shortText: v.annotation[0],
-                      text: v.annotation
-                    } );
+                if (v.annotation !== "") {
+                    annotations.push({
+                        series: 'µSv/h',
+                        x: v.timestamp * 1000,
+                        shortText: v.annotation[0],
+                        text: v.annotation
+                    });
                 }
-                data_ld.push([ts,v.data.edr,v.data.edr_avg]);
+                data_ld.push([ts, v.data.edr, v.data.edr_avg]);
                 edr_avg += v.data.edr;
 
             });
 
             // Update rolling 24h average EDR for alert reference
-            edr_avg_24 = (edr_avg/msg.log.length);
+            edr_avg_24 = (edr_avg / msg.log.length);
         }
 
         //FIXME: this is very redundant with set_age, refactor...
         var age = my.chart_age;
-        if (age > 60*60*1) {
+        if (age > 60 * 60 * 1) {
             data = data_ld;
             //console.log("LD")
         } else {
             data = data_hd;
             //console.log("HD",webGI.log.data.length)
         }
-        if (chart === null) {
+        if (!chart) {
             my.init();
         } else {
             chart.updateOptions({
                 file: data,
-                dateWindow: [ my.now - my.chart_age*1000, my.now]
+                dateWindow: [my.now - my.chart_age * 1000, my.now]
             });
         }
         //console.log(annotations);
@@ -244,12 +239,16 @@ function objToString (obj) {
 
 
     my.set_log_scale = function(enabled) {
-        if (chart) chart.updateOptions({ logscale: enabled });
+        if (chart) chart.updateOptions({
+            logscale: enabled
+        });
         my.log_scale = enabled;
     };
 
     my.set_colors = function(c) {
-        if (chart) chart.updateOptions({ colors: c });
+        if (chart) chart.updateOptions({
+            colors: c
+        });
         my.chart_colors = c;
     };
 
@@ -259,15 +258,13 @@ function objToString (obj) {
             clearTimeout(animtimer);
         }
 
-
-        if (seconds > 60*60*1) {
+        if (seconds > 60 * 60 * 1) {
             data = data_ld;
         } else {
             data = data_hd;
         }
 
-        if (chart === null)
-        {
+        if (!chart) {
             my.init();
         } else {
             chart.updateOptions({
@@ -275,7 +272,8 @@ function objToString (obj) {
                 dateWindow: desired_range
             });
         }
-        zoom(my.chart_age*1000);
+
+        zoom(my.chart_age * 1000);
         chart.setAnnotations(annotations);
     };
 
@@ -287,10 +285,10 @@ function objToString (obj) {
         container.hide();
     };
 
-    my.requestLog=function(age,hd) {
+    my.requestLog = function(age, hd) {
         var cmd = {
-            "cmd" : "read",
-            "age" : age,
+            "cmd": "read",
+            "age": age,
             "hd": hd
         };
 
@@ -298,11 +296,11 @@ function objToString (obj) {
         //console.log ("Requesting log (age " +webGI.log.chart_age +" )");
     };
 
-    my.requestHistory= function(from,to) {
+    my.requestHistory = function(from, to) {
         var cmd = {
-            "cmd" : "history",
-            "from" : from,
-            "to" : to
+            "cmd": "history",
+            "from": from,
+            "to": to
         };
 
         ws_log.send(JSON.stringify(cmd));
@@ -310,23 +308,23 @@ function objToString (obj) {
     };
 
     my.getDoseRateAvg15m = function() {
-        return data_hd[data_hd.length-1][2];
+        return data_hd[data_hd.length - 1][2];
     };
 
     my.getDose24h = function() {
         var first = data_ld_all[0];
-        var last = data_ld_all[data_ld_all.length-1];
-        var counts = last.data.totalcount_dtc-first.data.totalcount_dtc;
-        var cpm = counts/(24*60);
+        var last = data_ld_all[data_ld_all.length - 1];
+        var counts = last.data.totalcount_dtc - first.data.totalcount_dtc;
+        var cpm = counts / (24 * 60);
         var d24h = cpm * last.parameters.tube_factor * 24;
         return parseFloat(d24h.toFixed(3));
     };
 
 
-    my.pushAnnotation=function(ts,text) {
+    my.pushAnnotation = function(ts, text) {
         var cmd = {
-            "cmd" : "annotation",
-            "timestamp" : ts,
+            "cmd": "annotation",
+            "timestamp": ts,
             "text": text
         };
 
@@ -334,31 +332,35 @@ function objToString (obj) {
         //console.log ("Requesting history");
     };
 
-    //Private Function
+    // Private functions
+
     function chartApproachRange() {
         if (!desired_range) return;
         // go halfway there
         var range = chart.xAxisRange();
         if (Math.abs(desired_range[0] - range[0]) < 60 &&
             Math.abs(desired_range[1] - range[1]) < 60) {
-            if (my.chart_age > 60*60*1) {
+            if (my.chart_age > 60 * 60 * 1) {
                 data = data_ld;
             } else {
                 data = data_hd;
             }
-        chart.setAnnotations(annotations);
-        chart.updateOptions({
-            dateWindow: desired_range,
-            file: data
-        });
-          // (do not set another timeout.)
+            chart.setAnnotations(annotations);
+            chart.updateOptions({
+                dateWindow: desired_range,
+                file: data
+            });
+            // (do not set another timeout.)
         } else {
-          var new_range;
-          new_range = [0.5 * (desired_range[0] + range[0]),
-                       0.5 * (desired_range[1] + range[1])];
-          chart.setAnnotations(annotations);
-          chart.updateOptions({dateWindow: new_range});
-          chartAnimate();
+            var new_range;
+            new_range = [0.5 * (desired_range[0] + range[0]),
+                0.5 * (desired_range[1] + range[1])
+            ];
+            chart.setAnnotations(annotations);
+            chart.updateOptions({
+                dateWindow: new_range
+            });
+            chartAnimate();
         }
     }
 
@@ -366,12 +368,12 @@ function objToString (obj) {
         animtimer = setTimeout(chartApproachRange, 50);
     }
 
-    function zoom (age) {
+    function zoom(age) {
         var w = chart.xAxisRange();
-        desired_range = [ my.now-age, my.now];
+        desired_range = [my.now - age, my.now];
         chartAnimate();
     }
 
-    //Do not forget to return my, otherwise nothing will work.
     return my;
-}($));  //Pass jq/zepto to the module construction function call
+
+}($));
